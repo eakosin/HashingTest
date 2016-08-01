588abf48599022d606441fe52717141428cd023b
Evan Kosin <eakosin@gmail.com>
2016-07-31 21:15:24 -0400

    Initial Release Changes
    Added comments, refined layout and UI, moved hashing to thread and added
    cancel ability, added an icon.

--- HashingTest/HashingTest.cs
+++ HashingTest/HashingTest.cs
@@ -2,117 +2,154 @@
 using System;
 using System.Diagnostics;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace HashTest
 {
-    public partial class HashingTest : Form
-    {
-        public HashingTest()
-        {
-            InitializeComponent();
-        }
+	public partial class HashingTest : Form
+	{
+		private Thread hashingThread;
 
-        private void scryptUpdateStatistics(object sender, EventArgs e)
-        {
-            scryptMemoryCostTextbox.Text = "";
-            scryptBufferMixCoverageTextbox.Text = "";
-            try
-            {
-                int cost = Int32.Parse(scryptCostTextbox.Text);
-                int blockSize = Int32.Parse(scryptBlockSizeTextbox.Text);
-                int memoryUsageBytes = (128 * cost * blockSize);
-                string memoryUsageString;
-                if (memoryUsageBytes >= 1073741824)
-                {
-                    memoryUsageString = ((double)memoryUsageBytes / 1073741824.0).ToString() + " GB";
-                }
-                else if (memoryUsageBytes >= 1048576)
-                {
-                    memoryUsageString = ((double)memoryUsageBytes / 1048576.0).ToString() + " MB";
-                }
-                else if (memoryUsageBytes >= 1024)
-                {
-                    memoryUsageString = ((double)memoryUsageBytes / 1024.0).ToString() + " KB";
-                }
-                else
-                {
-                    memoryUsageString = memoryUsageBytes.ToString() + " bytes";
-                }
-                scryptMemoryCostTextbox.Text = memoryUsageString;
+		public HashingTest()
+		{
+			InitializeComponent();
+			scryptUpdateStatistics(null, null);
+		}
 
-                if ((1024.0 * (double)blockSize) > ((double)cost * Math.Log((double)cost)))
-                {
-                    scryptBufferMixCoverageTextbox.Text = "Buffer is under mixed: 1024r > NlogN";
-                }
-                else
-                {
-                    scryptBufferMixCoverageTextbox.Text = "Good";
-                }
-            }
-            catch
-            {
-                // Do nothing...
-            }
-        }
+		private void scryptUpdateStatistics(object sender, EventArgs e)
+		{
+			// Get scrypt parameters
+			long cost = (long)scryptCostValue.Value;
+			long blockSize = (long)scryptBlockSizeValue.Value;
 
-        private void StartButton_Click(object sender, EventArgs e)
-        {
-            StartButton.Enabled = false;
-            HashingTimeTextbox.Text = "";
-            HashResultTextbox.Text = "";
-            Stopwatch stopwatch = new Stopwatch();
-            if (scryptOption.Checked)
-            {
-                int cost, blockSize, parallel, derivedKeyLength;
-                try
-                {
-                    cost = Int32.Parse(scryptCostTextbox.Text);
-                    blockSize = Int32.Parse(scryptBlockSizeTextbox.Text);
-                    parallel = Int32.Parse(scryptParallelTextbox.Text);
-                    derivedKeyLength = Int32.Parse(scryptDerivedKeyLengthTextbox.Text);
-                }
-                catch
-                {
-                    HashResultTextbox.Text = "Error - scrypt parameters failed to parse.";
-                    StartButton.Enabled = true;
-                    return;
-                }
-                byte[] hashResultBytes;
-                byte[] passwordBytes = Encoding.UTF8.GetBytes(PasswordTextBox.Text);
-                byte[] saltBytes = Encoding.UTF8.GetBytes(SaltTextBox.Text);
-                stopwatch.Start();
-                hashResultBytes = SCrypt.ComputeDerivedKey(passwordBytes, saltBytes, cost, blockSize, parallel, null, derivedKeyLength);
-                stopwatch.Stop();
-                HashingTimeTextbox.Text = stopwatch.ElapsedMilliseconds.ToString() + " ms";
-                HashResultTextbox.Text = "SCRYPT:" + cost.ToString() + ":" + blockSize.ToString() + ":" + parallel.ToString() + ":" + Convert.ToBase64String(saltBytes) + ":" + Convert.ToBase64String(hashResultBytes);
-            }
-            else
-            {
-                int rounds;
-                try
-                {
-                    rounds = Int32.Parse(bcryptRoundsTextbox.Text);
-                }
-                catch
-                {
-                    HashResultTextbox.Text = "Error - bcrypt parameters failed to parse.";
-                    StartButton.Enabled = true;
-                    return;
-                }
-                string hashResult;
-                string salt = (SaltTextBox.Text.Length > 22) ? SaltTextBox.Text.Substring(0, 22) : SaltTextBox.Text;
-                byte[] saltBytes = new byte[22];
-                Encoding.UTF8.GetBytes(salt).CopyTo(saltBytes, 0);
-                string prefixSalt = "$2a$" + rounds.ToString("00") + "$" + Convert.ToBase64String(saltBytes);
-                stopwatch.Start();
-                hashResult = BCrypt.Net.BCrypt.HashPassword(PasswordTextBox.Text, prefixSalt);
-                stopwatch.Stop();
-                HashingTimeTextbox.Text = stopwatch.ElapsedMilliseconds.ToString() + " ms";
-                HashResultTextbox.Text = hashResult;
-            }
-            StartButton.Enabled = true;
-        }
+			// Calculate scrypt memory usage: 128 * N * r
+			long memoryUsageBytes = (128 * cost * blockSize);
+			string memoryUsageString;
 
-    }
+			// Make the value pretty.
+			if (memoryUsageBytes >= 1073741824)
+			{
+				memoryUsageString = ((double)memoryUsageBytes / 1073741824.0).ToString() + " GB";
+			}
+			else if (memoryUsageBytes >= 1048576)
+			{
+				memoryUsageString = ((double)memoryUsageBytes / 1048576.0).ToString() + " MB";
+			}
+			else if (memoryUsageBytes >= 1024)
+			{
+				memoryUsageString = ((double)memoryUsageBytes / 1024.0).ToString() + " KB";
+			}
+			else
+			{
+				memoryUsageString = memoryUsageBytes.ToString() + " bytes";
+			}
+			scryptMemoryCostTextbox.Text = memoryUsageString;
+
+			// Check if the buffer is under-mixed:
+			// http://stackoverflow.com/questions/11126315/what-are-optimal-scrypt-work-factors/33297994#33297994
+			if ((1024.0 * (double)blockSize) > ((double)cost * Math.Log((double)cost)))
+			{
+				scryptBufferMixCoverageTextbox.Text = "Buffer is under mixed: 1024r > NlogN";
+			}
+			else
+			{
+				scryptBufferMixCoverageTextbox.Text = "Good";
+			}
+		}
+
+		private void StartButton_Click(object sender, EventArgs e)
+		{
+			if (StartButton.Text == "Start")
+			{
+				// Start thread
+				hashingThread = new Thread(new ThreadStart(DoTheHashing));
+				hashingThread.Start();
+			}
+			else
+			{
+				// Abort thread
+				hashingThread.Abort();
+
+				// Cleanup
+				hashingThread = null;
+				GC.Collect();
+
+				// Reset UI elements
+				StartButton.Text = "Start";
+				PasswordSaltLayoutTable.Enabled = true;
+				scryptConfigurationContainer.Enabled = true;
+				bcryptConfigurationContainer.Enabled = true;
+			}
+		}
+
+		private void DoTheHashing()
+		{
+			// Disable and clear UI elements
+			PasswordSaltLayoutTable.Enabled = false;
+			scryptConfigurationContainer.Enabled = false;
+			bcryptConfigurationContainer.Enabled = false;
+			HashingTimeTextbox.Text = "";
+			HashResultTextbox.Text = "";
+
+			Stopwatch stopwatch = new Stopwatch();
+
+			if (scryptOption.Checked)
+			{
+				// Get scrypt parameters
+				int cost, blockSize, parallel, derivedKeyLength;
+				cost = (int)scryptCostValue.Value;
+				blockSize = (int)scryptBlockSizeValue.Value;
+				parallel = (int)scryptParallelValue.Value;
+				derivedKeyLength = (int)scryptDerivedKeyLengthValue.Value;
+
+				byte[] hashResultBytes;
+
+				// Build UTF8 byte arrays from password and salt
+				byte[] passwordBytes = Encoding.UTF8.GetBytes(PasswordTextBox.Text);
+				byte[] saltBytes = Encoding.UTF8.GetBytes(SaltTextBox.Text);
+
+				// Do the hashing
+				stopwatch.Start();
+				hashResultBytes = SCrypt.ComputeDerivedKey(passwordBytes, saltBytes, cost, blockSize, parallel, null, derivedKeyLength);
+				stopwatch.Stop();
+
+				// Display results
+				HashingTimeTextbox.Text = stopwatch.ElapsedMilliseconds.ToString() + " ms";
+				HashResultTextbox.Text = "SCRYPT:" + cost.ToString() + ":" + blockSize.ToString() + ":" + parallel.ToString() + ":" + Convert.ToBase64String(saltBytes) + ":" + Convert.ToBase64String(hashResultBytes);
+			}
+			else
+			{
+				// Get bcrypt parameters
+				int rounds;
+				rounds = (int)bcryptRoundsValue.Value;
+
+				string hashResult;
+
+				// Trim or pad salt to 22 bytes, base64 encode, and prefix parameters
+				string salt = (SaltTextBox.Text.Length > 22) ? SaltTextBox.Text.Substring(0, 22) : SaltTextBox.Text;
+				byte[] saltBytes = new byte[22];
+				Encoding.UTF8.GetBytes(salt).CopyTo(saltBytes, 0);
+				string prefixSalt = "$2a$" + rounds.ToString("00") + "$" + Convert.ToBase64String(saltBytes);
+
+				// Do the hashing
+				stopwatch.Start();
+				hashResult = BCrypt.Net.BCrypt.HashPassword(PasswordTextBox.Text, prefixSalt);
+				stopwatch.Stop();
+
+				// Display results
+				HashingTimeTextbox.Text = stopwatch.ElapsedMilliseconds.ToString() + " ms";
+				HashResultTextbox.Text = hashResult;
+			}
+			// Cleanup
+			GC.Collect();
+
+			// Reset UI elements
+			StartButton.Text = "Start";
+			PasswordSaltLayoutTable.Enabled = true;
+			scryptConfigurationContainer.Enabled = true;
+			bcryptConfigurationContainer.Enabled = true;
+		}
+
+	}
 }
